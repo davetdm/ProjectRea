a34bc2fe7d9cfaf8f87f79ecb9f3e03235f6e59a
REAMANZINI <reamanzini@gmail.com>
2019-09-12 14:16:08 +0200

    pushed the database

--- application/views/about.php
+++ application/views/about.php
@@ -49,7 +49,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                 <div class="col-md-4 ab-content text-center p-lg-5 p-3 my-lg-5">
                     <h4>Travel Must Haves</h4>
                     <p>Lorem ipsum dolor sit,Nulla pellentesque dolor ipsum laoreet eleifend integer,Pellentesque maximus libero.</p>
-                    <a href="shop.php" class="btn shop mt-3">Shop Now</a>
+                    <a href="<?php echo base_url(); ?>collections" class="btn shop mt-3">Shop Now</a>
 
                 </div>
             </div>
@@ -70,7 +70,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                 <div class="col-md-4 content-gd-wthree ab-content py-lg-5 my-lg-5">
                     <h4>Need Extra Space ?</h4>
                     <p>Lorem ipsum dolor sit,Nulla pellentesque dolor ipsum laoreet eleifend integer,Pellentesque maximus libero.</p>
-                    <a href="shop.php" class="btn shop mt-3">Shop Now</a>
+                    <a href="<?php echo base_url(); ?>collections" class="btn shop mt-3">Shop Now</a>
 
                 </div>
                 <div class="col-md-4 content-gd-wthree">
@@ -88,14 +88,14 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                 <img src="<?php echo base_url(); ?>assets/images/ab1.jpg" class="img-fluid" alt="" />
                 <div class="pos-wthree">
                     <h4 class="text-wthree">50% Off Any <br> Women's Bags</h4>
-                    <a href="shop.php" class="btn shop mt-3">Shop Now</a>
+                    <a href="<?php echo base_url(); ?>collections" class="btn shop mt-3">Shop Now</a>
                 </div>
             </div>
             <div class="col-lg-6 bottom-w3pvt-left bottom-w3pvt-right p-lg-0">
                 <img src="<?php echo base_url(); ?>assets/images/ab2.jpg" class="img-fluid" alt="" />
                 <div class="pos-w3pvt">
                     <h4 class="text-w3pvt">30% Off Any <br> Men's Bags</h4>
-                    <a href="shop.php" class="btn shop mt-3">Shop Now</a>
+                    <a href="<?php echo base_url(); ?>collections" class="btn shop mt-3">Shop Now</a>
                 </div>
             </div>
         </div>
