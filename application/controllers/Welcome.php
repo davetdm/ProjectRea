80c97a1c8a1b6142d1f277102580cb1150167c01
Johannes Ramothale <jlramothale@gmail.com>
2019-09-13 15:08:44 +0200

    Added model testing and all

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -6,25 +6,41 @@ class Welcome extends CI_Controller {
 	public function __construct() {
 		parent::__construct();
 		$this->load->helper('url');
+		$this->load->form();
 	}
 
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see https://codeigniter.com/user_guide/general/urls.html
-	 */
 	public function index()
 	{
+		// $this->load->database();
+
+		$data = array(
+			"id" => 1,
+			"name" => "Test",
+			"price" => "500",
+			"color" => "Red",
+			"picture" => ""
+		);
+
+		// specify the table<product> and $data
+		// $this->db->insert('product', $data);
+		$result = $this->WelcomeModel->add_product($data);
+		if ($result == true){
+			echo "insert successful";
+		} else {
+			var_dump($result);
+		}
 		$this->load->view('welcome_message');
+	}
+
+	public function products(){
+		// Load model
+		// $this->load->model('WelcomeModel');
+		$users = $this->WelcomeModel->get_products();
+		// echo "<pre/>";
+		foreach($users as $user){
+			echo "Name: ". $user["color"] . "<br/>";
+		}
+		// print_r($users);
+		
 	}
 }
